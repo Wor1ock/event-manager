7dca0ad7c401711c6134ae4dc2e797a0bb47d9a3
Wor1ock <vanya.yankin9@mail.ru>
2024-03-23 18:07:07 +0400

    feat: добавить контроллера для вывода данных Events из бд на страницу

--- src/main/java/com/afisha/event_manager/controllers/EventController.java
+++ src/main/java/com/afisha/event_manager/controllers/EventController.java
@@ -1,14 +1,20 @@
 package com.afisha.event_manager.controllers;
 
+import com.afisha.event_manager.models.Events;
+import com.afisha.event_manager.repositories.EventsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EventController {
-@GetMapping("/Events")
-        public String eventMain(Model model) {
-    return "event-main";
-
-}
+    @Autowired
+    private EventsRepository eventsRepository;
+    @GetMapping("/Events")
+    public String eventMain(Model model) {
+        Iterable<Events> events = eventsRepository.findAll();
+        model.addAttribute("events", events);
+        return "event-main";
+    }
 }
