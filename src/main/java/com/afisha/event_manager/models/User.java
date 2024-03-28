694591c1d6572767e2079a7b37a9c87bae36b43e
Wor1ock <vanya.yankin9@mail.ru>
2024-03-28 18:04:16 +0400

    feat: добавить роли и внедрить их в юзера

--- src/main/java/com/afisha/event_manager/models/User.java
+++ src/main/java/com/afisha/event_manager/models/User.java
@@ -1,25 +1,67 @@
 package com.afisha.event_manager.models;
 
+import com.afisha.event_manager.models.enums.Role;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class User {
+@Table(name = "users")
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
     @Column(name = "username")
     private String username;
-    @Column(name = "email")
+    @Column(name = "email", unique = true)
     private String email;
     @Column(name = "password")
     private String password;
-    @Column(name = "role")
-    private Boolean role;
+    @Column(name = "date_of_creation")
+    private LocalDateTime dateOfCreation;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles = new HashSet<>();
+
+    @PrePersist
+    private void init() {
+        dateOfCreation = LocalDateTime.now();
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
